63feb65b97b35bc53b52dffadae48b2434dc35f9
Amy Grace <zgracem@users.noreply.github.com>
2023-12-18 13:30:43 -0700

    completions: improve `defaults` (macOS config manager)

--- share/completions/defaults.fish
+++ share/completions/defaults.fish
@@ -4,30 +4,36 @@ function __fish_defaults_domains
     defaults domains | string split ", "
 end
 
-complete -f -c defaults -o currentHost -d 'Restricts preferences operations to the current logged-in host'
-complete -f -c defaults -o host -d 'Restricts preferences operations to hostname'
+complete -f -c defaults -o currentHost -d 'Only operate on this host'
+complete -f -c defaults -o host -xf -d 'Only operate on HOST'
+
+# subcommands which take a domain as their first argument
+set -l defaults_domain_cmds read read-type write rename delete import export
 
 # read
-complete -f -c defaults -n __fish_use_subcommand -a read -d 'Shows defaults entire given domain'
-complete -f -c defaults -n '__fish_seen_subcommand_from read read-type write rename delete' -a '(__fish_defaults_domains)'
-complete -f -c defaults -n '__fish_seen_subcommand_from read read-type write rename delete' -o app
+complete -f -c defaults -n __fish_use_subcommand -a read -d 'Print defaults'
+complete -f -c defaults -n "__fish_seen_subcommand_from $defaults_domain_cmds" -x -a '(__fish_defaults_domains)'
+complete -f -c defaults -n "__fish_seen_subcommand_from $defaults_domain_cmds" -o app -x
+complete -f -c defaults -n "__fish_seen_subcommand_from $defaults_domain_cmds" -s g -o globalDomain
 
 # write
 complete -f -c defaults -n __fish_use_subcommand -a write -d 'Writes domain or or a key in the domain'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o string -d 'String as the value for the given key'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o data -d 'Raw data bytes for given key'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o int -d 'Integer as the value for the given key'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o float -d 'Floating point number as the value for the given key'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o bool -d 'Boolean as the value for the given key'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o date -d 'Date as the value for the given key'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o array -d 'Array as the value for the given key'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o array-add -d 'Add new elements to the end of an array'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o dict -d 'Add a dictionary to domain'
-complete -f -c defaults -n '__fish_seen_subcommand_from write' -o dict-add -d 'Add new key/value pairs to a dictionary'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o string -d 'String'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o data -d 'Raw data bytes'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o int -d 'Integer'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o float -d 'Floating point number'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o bool -d 'Boolean'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o date -d 'Date'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o array -d 'Array'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o array-add -d 'Append new elements'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o dict -d 'Dictionary'
+complete -f -c defaults -n '__fish_seen_subcommand_from write' -o dict-add -d 'Add new key/value pairs'
 
-complete -f -c defaults -n __fish_use_subcommand -a read-type -d 'Shows the type for the given domain, key'
-complete -f -c defaults -n __fish_use_subcommand -a rename -d 'Renames old_key to new_key'
-complete -f -c defaults -n __fish_use_subcommand -a delete -d 'Deletes domain or a key in the domain'
-complete -f -c defaults -n __fish_use_subcommand -a domains -d 'Prints the names of all domains in the users defaults system'
+complete -f -c defaults -n __fish_use_subcommand -a read-type -d 'Print the type of a key in a domain'
+complete -f -c defaults -n __fish_use_subcommand -a rename -d 'Rename old_key to new_key in a domain'
+complete -f -c defaults -n __fish_use_subcommand -a delete -d 'Delete a domain or a key in a domain'
+complete -f -c defaults -n __fish_use_subcommand -a import -d 'Import to a domain'
+complete -f -c defaults -n __fish_use_subcommand -a export -d 'Export a domain'
+complete -f -c defaults -n __fish_use_subcommand -a domains -d 'Print all domains'
 complete -f -c defaults -n __fish_use_subcommand -a find -d 'Searches for word in domain names, keys, and values'
 complete -f -c defaults -n __fish_use_subcommand -a help -d 'Prints a list of possible command formats'
