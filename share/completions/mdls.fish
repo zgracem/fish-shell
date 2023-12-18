f3acc91b85e58205e4730c0b48a749ab1c986c97
Amy Grace <zgracem@users.noreply.github.com>
2023-12-18 13:32:44 -0700

    completions: improve `mdls` (macOS metadata utility)

--- share/completions/mdls.fish
+++ share/completions/mdls.fish
@@ -1,6 +1,7 @@
 # completion for mdls (macOS)
 
-complete -c mdls -s n -o name -x -d 'Print only the matching metadata attribute value'
-complete -c mdls -s r -o raw -f -d 'Print raw attribute data'
-complete -c mdls -n '__fish_seen_subcommand_from -raw -r' -o nullMarker -x -d 'Sets a marker string to be used when a requested attribute is null'
-complete -c mdls -s p -o plist -r -d 'Output attributes in XML format to file'
+complete -c mdls -s n -o attr -x -d 'Fetch attribute NAME'
+complete -c mdls -s r -o raw -d 'Don\'t print attribute names'
+complete -c mdls -n '__fish_seen_subcommand_from -raw -r' -o nullMarker -x -d 'String to mark null attributes'
+complete -c mdls -s p -o plist -rF -d 'Output in XML format to file'
+complete -c mdls -s s -o sdb -d 'Implies -attr _kMDItemSDBInfoexample'
